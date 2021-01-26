c1aa7a8889a99ed320e80ef3a36ab8a3840adb4c
trungdc-ha <dangchitrungha@gmail.com>
2021-01-26 09:43:58 +0700

    first commit

---
+++ src/main/java/vn/codegym/king/KingApplication.java
@@ -0,0 +1,13 @@
+package vn.codegym.king;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KingApplication.class, args);
+    }
+
+}
---
+++ src/main/java/vn/codegym/king/controllers/HomeController.java
@@ -0,0 +1,12 @@
+package vn.codegym.king.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping
+    public String showHomePage(){
+        return "index";
+    }
+}
